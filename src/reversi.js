4b3c45f05a08cfefe4fc5f6bb9c95dec0a13939e
Han Jie Lee <hjl386@nyu.edu>
2017-02-07 19:49:53 -0500

    5 Passing Test, 4 being created as of now

--- src/reversi.js
+++ src/reversi.js
@@ -26,16 +26,43 @@ function rowColToIndex(board, rowNumber, columnNumber){
 }
 
 function indexToRowCol(board, i){
+//Returns an object with the properties row and col representing the row and column numbers that the index maps to from the passed in board (array)
 	const size = Math.sqrt(board.length);
 	const rowNum = Math.floor(i/size);
 	const colNum = i % size;
-	const boardRC = {row: rowNum, column: colNum};
+	const boardRC = {row: rowNum, col: colNum};
 	return boardRC;
+}
+
+function setBoardCell(board, letter, row, col){
+//Returns a single dimensional array, board, with the value at the provided row and col is set to letter 
+	//const array = board.slice(0, board.length);
+	const array = [...board];
+	const index = rowColToIndex(board, row, col);
+	array[index] = letter;
+	return array;
+}
+
+function algebraicToRowCol(algebraicNotation){
+	if(algebraicNotation.length !== 2){
+		return undefined;
+	} else if(isNan(algebraicNotation.charAt(1)){
+		return undefined;
+	} else if(typeof(c.charAt(0)) !== 'string'){
+		return undefined;
+	} else {
+		
+	}
 }
 
 module.exports = {
 	//The first term can be named anything, the second is the function name
 	repeat: repeat,
 	generateBoard: generateBoard,
-	rowColToIndex: rowColToIndex
+	rowColToIndex: rowColToIndex,
+	indexToRowCol: indexToRowCol,
+	setBoardCell: setBoardCell,
+	algebraicToRowCol: algebraicToRowCol
 }
+
+
